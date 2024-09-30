fe20a6379c38ea7269afce0db7b02b547b1f2607
Dan <daramirezp1112@gmail.com>
2024-09-29 23:09:29 -0500

    feat(decks): add custom hooks

--- src/types/deckTypes.ts
+++ src/types/deckTypes.ts
@@ -1,7 +1,9 @@
+import { Card } from "./cardTypes";
+
 export interface Deck {
     id: string;
     name: string;
     creator: string;
     desc: string;
-    cards: string[];
+    cards: Card[];
 }
