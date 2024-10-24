39448e128eec7430cac92f3f9d5e309e96141907
Dan <daramirezp1112@gmail.com>
2024-10-23 23:58:47 -0500

    feat(routes): refactor user logged in info

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Router from './routes/appRoutes';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-  <Router /> 
+  <App/>
   </Provider>
 );
--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,17 +1,20 @@
-import { createBrowserRouter, RouterProvider, Navigate, Outlet, Routes, Route } from 'react-router-dom';
+import { Navigate, Routes, Route, Router } from 'react-router-dom';
 import { HomePage, SearchPage } from '../pages/imports';
 import { Login, Navbar, Register } from '../components/imports';
 import { useState } from 'react';
 import { clearAuthUserId, getAuthUserId } from '../utils/storage';
-import MainPage from '../pages/MainPage/MainPage';
 import Catalogue from '../pages/Catalogue/Catalogue';
 import Purchases from '../pages/Purchases/Purchases';
 import About from '../pages/About/About';
 import Tournament from '../pages/TournamentPage/TournamentPage';
 import { useSelector } from 'react-redux';
+import ProtectedRoutes from './ProtectedRoutes';
+import Dashboard from '../pages/Dashboard/Dashboard';
 
 const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(getAuthUserId() ? true : false);
+  const [isLoggedIn, setIsLoggedIn] = useState(getAuthUserId() !== '' ? true : false);
+
+  console.log(isLoggedIn);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
@@ -21,6 +24,9 @@ const AppRouter = () => {
     setIsLoggedIn(false);
     clearAuthUserId();
   };
+
+  const user = useSelector((state) => state.user);
+  console.log(user);
 
   return (
     <Router>
@@ -30,82 +36,21 @@ const AppRouter = () => {
         
         <Routes>
           <Route path="/" element={<HomePage isLoggedIn={isLoggedIn} />} />
-          <Route path="/home" element={<HomePage isLoggedIn={isLoggedIn} />} />
+          <Route path="/dashboard" element={<Dashboard/>} />
 
-
-          <Route path="/catalogue" element={<Catalogue />} />
-          <Route path="/purchases" element={<Purchases />} />
-          <Route path="/tournaments" element={<Tournament />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/search" element={<SearchPage isLoggedIn={isLoggedIn} onLogout={handleLogout} />} />
-          <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />} />
-          <Route path="/register" element={isLoggedIn ? <Navigate to="/" /> : <Register onRegister={handleLogin} />} />
+          <Route element={<ProtectedRoutes isLoggedIn={isLoggedIn}/>}>
+            <Route path="/catalogue" element={<Catalogue />} />
+            <Route path="/purchases" element={<Purchases />} />
+            <Route path="/tournaments" element={<Tournament />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/search" element={<SearchPage isLoggedIn={isLoggedIn} onLogout={handleLogout} />} />
+            <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />} />
+            <Route path="/register" element={isLoggedIn ? <Navigate to="/" /> : <Register onRegister={handleLogin} />} />
+          </Route>
           </Routes>
       </>
     </Router>
   )
 }
 
-const Router = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(getAuthUserId() ? true : false);
-
-  const user = useSelector((state) => state.user);
-
-  console.log(user);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    clearAuthUserId();
-  };
-
-
-
-  const router = createBrowserRouter([
-    {
-      path: '/', 
-      element: <><Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout}/><Outlet/></>, 
-      children: [
-        {
-          index: true, 
-          element: <MainPage isLoggedIn={isLoggedIn}/>,
-        },
-        {
-          path: 'catalogue',
-          element: <Catalogue/>,
-        },
-        {
-          path: 'purchases',
-          element: <Purchases/>, 
-        },
-        {
-          path: 'tournaments',
-          element: <Tournament/>,
-        },
-        {
-          path: 'about',
-          element: <About/>,
-        }
-      ],
-    },
-    {
-      path: '/search',
-      element: <SearchPage isLoggedIn={isLoggedIn} onLogout={handleLogout} />,
-    },
-    {
-      path: '/login',
-      element: isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />,
-    },
-    {
-      path: '/register',
-      element: isLoggedIn ? <Navigate to="/" /> : <Register onRegister={handleLogin} />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-};
-
-export default Router;
+export default AppRouter;
