910523c4744f0c916bec81917ad2bc7e9da9ee78
Dan <daramirezp1112@gmail.com>
2024-10-24 18:19:00 -0500

    feat(store): persist user slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { userSlice } from '../features/auth/userSlice'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { userSlice } from '../features/auth/userSlice';
+
+const persistConfig = {
+  key: 'user',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userSlice.reducer)
 
 export const store = configureStore({
   reducer: {
-    user: userSlice.reducer,
+    user: persistedReducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+    },
+  }
+  )
 })
+
+export const persistor = persistStore(store)
