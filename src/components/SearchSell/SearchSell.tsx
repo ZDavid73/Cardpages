b3d9528cabae34bf7d9e5564f052648a0928675e
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-10-28 18:21:22 -0500

    Feat: new code on searchsell component

--- src/components/SearchSell/SearchSell.tsx
+++ src/components/SearchSell/SearchSell.tsx
@@ -1,38 +1,12 @@
-import React, { useState } from 'react';
-import SearchBar from '../SearchBar/SearchBar';
-import { Card } from '../../types/cardTypes';
-import './SearchSell.css';
+import React from 'react';
+import useSearchSell from '../../hooks/useSearchSell';
 
 const SearchSell = () => {
-  const [selectedCards, setSelectedCards] = useState<Card[]>([]);
-
-  const handleCardClick = (card: Card) => {
-    if (!selectedCards.some((selectedCard) => selectedCard.id === card.id)) {
-      setSelectedCards((prevCards) => [...prevCards, card]);
-    }
-  };
+  const { query, results, error, selectedCards, setQuery, handleCardClick } = useSearchSell();
 
   return (
-    <div className="search-sell-container">
-      <h1>Busca y selecciona tu carta</h1>
-
-      <SearchBar onCardClick={handleCardClick} />
-
-      <div className="selected-cards">
-        <h2>Cartas seleccionadas:</h2>
-        <div className="card-grid">
-          {selectedCards.map((card) => (
-            <div key={card.id} className="card-item">
-              <img src={card.images.small} alt={card.name} className="card-image" />
-              <p>{card.name}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    
   );
 };
 
 export default SearchSell;
-
-
