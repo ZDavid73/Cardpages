801f9e9e3fb5dfe6bfb477a419bacbe84b2b7f7d
Dan <daramirezp1112@gmail.com>
2024-10-28 19:11:05 -0500

    feat(modal): add modal hooks

--- src/features/modalSlice.ts
+++ src/features/modalSlice.ts
@@ -25,4 +25,6 @@ export const modalSlice = createSlice({
   }
 })
 
+export const { openModal, closeModal } = modalSlice.actions
+
 export default modalSlice.reducer
--- src/types/stateType.ts
+++ src/types/stateType.ts
@@ -1,5 +1,7 @@
 import { UserState } from "../features/auth/userSlice";
+import { ModalState } from "../features/modalSlice";
 
 export interface AppState {
-    user: UserState
+    user: UserState,
+    modal: ModalState
 }
