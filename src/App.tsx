68c5034bb997ac957f7a77a849df47ea5e51e43b
Dan <daramirezp1112@gmail.com>
2024-10-30 19:52:26 -0500

    feat(tournaments): add fetch function in mounting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AppRouter from "./routes/appRoutes";
 import { useEffect, useMemo } from "react";
 import { getAuthUserId } from "./utils/storage";
 import { getUserInfo } from "./services/databaseService";
 import { login } from "./features/auth/userSlice";
+import { AppState } from "./types/stateType";
+import { fetchAllTournaments } from "./features/tournamentSlice";
 
 const App = () => {
     const dispatch = useDispatch();
-
     const userId = useMemo(() => getAuthUserId(), []);
+
+    //PENDIENTE: hacer ese mundo de fetchs en otro archivo y llamarlo desde acá
+
+    useEffect(() => {
+        dispatch(fetchAllTournaments())
+    }, )
 
     useEffect(() => {
         if (userId){
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { userSlice } from '../features/auth/userSlice';
 import { modalSlice } from '../features/modalSlice';
+import { tournamentSlice } from '../features/tournamentSlice';
 
 const persistConfig = {
   key: 'user',
@@ -15,6 +16,7 @@ export const store = configureStore({
   reducer: {
     user: persistedReducer,
     modal: modalSlice.reducer,
+    tournaments: tournamentSlice.reducer
   },
   devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
