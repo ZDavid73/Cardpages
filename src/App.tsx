b8ba4cc8743a605bdc53af79051c8133d024493f
Dan <daramirezp1112@gmail.com>
2024-10-31 11:51:05 -0500

    feat(tournaments): add update tournaments on supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import AppRouter from "./routes/appRoutes";
 import { useEffect, useMemo } from "react";
 import { getAuthUserId } from "./utils/storage";
 import { getUserInfo } from "./services/databaseService";
 import { login } from "./features/auth/userSlice";
-import { AppState } from "./types/stateType";
 import { fetchAllTournaments } from "./features/tournamentSlice";
+import { AppDispatch } from "./store/store";
 
 const App = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<AppDispatch>();
     const userId = useMemo(() => getAuthUserId(), []);
 
     //PENDIENTE: hacer ese mundo de fetchs en otro archivo y llamarlo desde acá
--- src/store/store.ts
+++ src/store/store.ts
@@ -27,4 +27,7 @@ export const store = configureStore({
   )
 })
 
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+
 export const persistor = persistStore(store)
