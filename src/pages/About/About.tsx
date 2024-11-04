7bfed0bd63fa97065d2c6d8d1c393df15bc3e4c9
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-04 12:00:01 -0500

    Refactor: Remove unused state for selectedCard in About component

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,7 @@
 import Profile from "../../components/Profile/Profile";
 import { Text } from "../../theme/styledcomponents";
-import { Card } from '../../types/cardTypes';
 
 const About = () => {
-    const [selectedCard, setSelectedCard] = useState<Card | null>(null);
-
     return (
         <>
         <Profile/>
