4e94ccd902944828118fcca23b380a07330cf468
Dan <daramirezp1112@gmail.com>
2024-11-05 11:46:30 -0500

    feat(store): add fetching cards on mounting

--- src/components/DataSync/DataSync.tsx
+++ src/components/DataSync/DataSync.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { supabase } from '../../services/supabaseClient';
 import { insertTourData, deleteTourData, fetchAllTournaments, updateTourData } from '../../features/tournamentSlice';
 import { AppDispatch } from '../../store/store';
 import { RealtimePostgresChangesPayload } from '@supabase/supabase-js';
+import { deleteCardData, insertCardData, updateCardData } from '../../features/cardSlice';
 
 const DataSync = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -29,7 +31,35 @@ const DataSync = () => {
     }
   }, [dispatch]);
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  useEffect(() => {
+    const subscriptionCard = supabase
+    .channel('cards')
+    .on(
+        'postgres_changes', 
+        { event: '*', schema: 'public', table: 'cards' },
+        (payload) => {
+          console.log('Change received:', payload);
+          handleChangeCard(payload);
+        }
+    )
+    .subscribe();
+
+    return () => {
+        subscriptionCard.unsubscribe();
+    }
+  }, [dispatch]);
+
+  const handleChangeCard = (payload: RealtimePostgresChangesPayload<{[key: string]: any;}>) => {
+    if (payload.eventType === 'INSERT') {
+      dispatch(insertCardData(payload.new))
+    }
+    if (payload.eventType === 'UPDATE') {
+      dispatch(updateCardData(payload.new))
+    }
+    if (payload.eventType === 'DELETE') {
+      dispatch(deleteCardData(payload.old))
+    }
+  }
   const handleChangeTournament = (payload: RealtimePostgresChangesPayload<{[key: string]: any;}>) => {
     if (payload.eventType === 'INSERT') {
       dispatch(insertTourData(payload.new))
