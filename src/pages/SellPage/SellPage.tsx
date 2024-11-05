859d4c241acf5af05d7df6f7d85ee57b4d7ed112
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-05 15:21:20 -0500

    Fix: Import SellPage CSS for styling enhancements

--- src/pages/SellPage/SellPage.tsx
+++ src/pages/SellPage/SellPage.tsx
@@ -2,6 +2,8 @@ import SearchSell from '../../components/SearchSell/SearchSell';
 import CardForm from '../../components/CardInfoSell/CardInfoSell';
 import { Card } from '../../types/cardTypes';
 import { useState } from 'react';
+import './SellPage.css';
+
 const SellPage = () => {
   
   const [selectedCard, setSelectedCard] = useState<Card | null>(null);
