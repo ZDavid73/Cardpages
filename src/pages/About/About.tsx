c547e82592ddc33367832c120b1c855175dc946c
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 16:53:33 -0500

    feat(AboutPage): remove unused useModal hook and clean up About component

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,12 @@
 import Profile from "../../components/Profile/Profile";
-import useModal from "../../hooks/useModal";
 import { Button, Text } from "../../theme/styledcomponents";
 import Footer from "../../components/Footer/Footer";
 const AboutPage = () => {
-    const { handleOpen} = useModal()
+    
     return (
         <>
         <Profile/>
-        <Text variant='white'>About</Text>
-        <Button variant='purple' onClick={()=>handleOpen('createCard')}>Click me</Button>
+        
         <Footer/>
         </>
     );
