d2611d9170258e41b270cadbe7980ec792b5d693
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 15:20:49 -0500

    feat(SellPage): add scrollbar color and overlay click handler for improved UX

--- src/pages/SellPage/SellPage.tsx
+++ src/pages/SellPage/SellPage.tsx
@@ -1,18 +1,23 @@
 import SearchSell from '../../components/SearchSell/SearchSell';
 import CardForm from '../../components/CardInfoSell/CardInfoSell';
+import { FaChevronLeft } from "react-icons/fa";
 import { Tittle } from '../../theme/styledcomponents';
+import useModal from '../../hooks/useModal';
 import { Card } from '../../types/cardTypes';
 import { useState } from 'react';
 import './SellPage.css';
 
 const SellPage = () => {
-  
+  const {handleClose } = useModal();
+    const handleOverlayClick = () => {
+        handleClose();
   const [selectedCard, setSelectedCard] = useState<Card | null>(null);
 
   return (
     <div className='sell-page'>
       <div className='search-sell-container'>
       <div className='headertittle'>
+      <FaChevronLeft onClick={handleOverlayClick} className='FaChevronLeft'/>
       <Tittle variant='white'>Sell Card</Tittle>
       </div>
       <SearchSell selectedCard={selectedCard} setSelectedCard={setSelectedCard} />
