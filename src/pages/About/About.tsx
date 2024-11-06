20743fcf9891f260c7f03adca8904ea3e7e96393
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 17:21:43 -0500

    fix(AboutPage): add detailed description to "What We Offer" section for clarity

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -14,6 +14,7 @@ const AboutPage = () => {
         <div className="About Content">
         <div className="Offer">
         <Tittle variant='white'>What We Offer</Tittle>
+        <Text variant='white'>At Capsule Corp, our services are designed to provide a complete experience for card game enthusiasts: Secure Card Trading: A dynamic marketplace where users can safely buy, sell, and trade cards, ensuring reliable and efficient transactions. Deck Builder: Intuitive tools that allow users to create and optimize their favorite decks, helping them refine their strategies and share them with other players. Tournament Organization: Advanced features to organize both local and online tournaments, with ranking systems, scheduling, and management tools that make events smooth and exciting.</Text>
         </div>
         <div className="Mission">
         <Tittle variant='white'>Our Mission</Tittle>
