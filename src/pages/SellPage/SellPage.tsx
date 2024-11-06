8c19b2deb005087c4a9355f8ed89ff1789dced9b
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 15:27:30 -0500

    feat(SellPage): add FaChevronLeft icon for enhanced navigation

--- src/pages/SellPage/SellPage.tsx
+++ src/pages/SellPage/SellPage.tsx
@@ -1,6 +1,7 @@
 import SearchSell from '../../components/SearchSell/SearchSell';
 import CardForm from '../../components/CardInfoSell/CardInfoSell';
 import { Tittle } from '../../theme/styledcomponents';
+import { FaChevronLeft } from "react-icons/fa";
 import { Card } from '../../types/cardTypes';
 import { useState } from 'react';
 import './SellPage.css';
