226224a028170a0b66573c6359012033d945b60a
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 21:28:46 -0500

    feat(SellingCardThumb): add SellingCardThumb component for displaying card details

---
+++ src/components/CardThumb/CardThum.tsx
@@ -0,0 +1,48 @@
+import { useSelector } from 'react-redux';
+import { Button, Container, Text } from '../../theme/styledcomponents';
+import { SellingCard } from '../../types/cardTypes';
+import { AppState } from '../../types/stateType';
+import { useNavigate } from 'react-router-dom';
+import './SellingCardThumb.css';
+
+type SellingCardThumbProps = {
+    card: SellingCard;
+}
+
+const SellingCardThumb = ({ card }: SellingCardThumbProps) => {
+    const userId = useSelector((state: AppState) => state.user.id);
+    const navigate = useNavigate();
+
+    return (
+        <section 
+            className='selling-card-thumbnail' 
+            style={{ 
+                backgroundImage: `url(${card.images.large})`, 
+                backgroundSize: 'cover', 
+                backgroundPosition: 'center' 
+            }}
+        >
+            <div className='card-button'>
+                {
+                    userId === card.sellerId ? 
+                    <Button 
+                        variant='purple' 
+                        onClick={() => navigate('/editCard', { state: { card } })}
+                    >
+                        Edit
+                    </Button>
+                    :
+                    <Button variant='gray' disabled>
+                        Sold
+                    </Button>
+                }
+            </div>
+            <Container variant='smallopacity'>
+                <Text variant='white'>{card.flavorText}</Text>
+                <Text variant='white'>${card.price}</Text>
+            </Container>
+        </section>
+    );
+}
+
+export default SellingCardThumb;
