fa040c5f463180dcbdfb8266e57a499bdf99001a
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 21:36:50 -0500

    feat(Catalogue): integrate SellingCardSection to display selling cards

--- src/pages/Catalogue/Catalogue.tsx
+++ src/pages/Catalogue/Catalogue.tsx
@@ -5,6 +5,7 @@ import Profile from "../../components/Profile/Profile"
 import AddButton from '../../components/AddButton/AddButton'
 import SellCardButton from '../../components/AddCard/AddCard'
 import { Tittle  } from '../../theme/styledcomponents';
+import SellingCardSection from '../../components/CardItem/CardItem';
 
 const Catalogue = () => {
     const sellingCards = useSelector((state: AppState) => state.sellingCards);
@@ -17,6 +18,7 @@ const Catalogue = () => {
         </div>
         <Tittle variant="white">Cards On Sale</Tittle>
         <SellCardButton/>
+        <SellingCardSection items={sellingCards.items} state={sellingCards.state}/>
         <AddButton/>
         </>
     )
