0bd0962cbbba8935e29168494e7ac929c1076ef3
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 23:59:11 -0500

    feat(Catalogue): enhance card display with loading and error messages

--- src/pages/Catalogue/Catalogue.tsx
+++ src/pages/Catalogue/Catalogue.tsx
@@ -1,27 +1,60 @@
 import './Catalogue.css'
 import { useSelector } from "react-redux";
+import { AppState } from "../../types/stateType";
 import Header from "../../components/Header/Header"
 import Profile from "../../components/Profile/Profile"
 import AddButton from '../../components/AddButton/AddButton'
 import SellCardButton from '../../components/AddCard/AddCard'
-import { Tittle  } from '../../theme/styledcomponents';
-import SellingCardSection from '../../components/CardItem/CardItem';
+import { Tittle, Text  } from '../../theme/styledcomponents';
 
 const Catalogue = () => {
-    const sellingCards = useSelector((state: AppState) => state.SellingCards);
+    const cardsState = useSelector((state: AppState) => state.cards);
 
     return (
         <>
-        <div className="catalogue-sectionheader">
-           <Profile/>
-            <Header/> 
-        </div>
-        <Tittle variant="white">Cards On Sale</Tittle>
-        <SellCardButton/>
-        <SellingCardSection items={sellingCards.items} state={sellingCards.state}/>
-        <AddButton/>
-        </>
-    )
-}
+            <div className="catalogue-sectionheader">
+                <Profile />
+                <Header />
+            </div>
+            <Tittle variant="white">Cards On Sale</Tittle>
 
-export default Catalogue
+            <SellCardButton />
+            <AddButton />
+
+            <section className="cards-section">
+                {/* Mostrar mensaje de carga */}
+                {cardsState.loading && <Text variant="white">Loading cards...</Text>}
+
+                {/* Mostrar mensaje de error */}
+                {cardsState.error && (
+                    <Text variant="white">Oh no! There was an error loading the cards. Try again later.</Text>
+                )}
+
+                {/* Mostrar mensaje cuando no hay cartas */}
+                {!cardsState.loading && !cardsState.error && cardsState.cards.length === 0 && (
+                    <Text variant="white">No cards available for sale.</Text>
+                )}
+
+                {/* Renderizar las imágenes de las cartas */}
+                {!cardsState.loading && !cardsState.error && cardsState.cards.length > 0 && (
+                    <div className="cards-container">
+                        {cardsState.cards.map((card) => (
+                            <div key={card.id} className="card-item">
+                                <img
+                                    src={card.images.small}
+                                    alt={`Card ${card.id}`}
+                                    className="card-image"
+                                />
+                                <Text variant="white">Price: ${card.price}</Text>
+                                <Text variant="white">{card.description}</Text>
+                                <button className="edit-button">Edit</button>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </section>
+        </>
+    );
+};
+
+export default Catalogue;
