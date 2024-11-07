02595d2e1bfdb4c8a4b5c868d6ad24b02aa0523f
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-07 14:27:27 -0500

    feat(SearchSell): add image handling for selected card in SearchSell component

--- src/components/SearchSell/SearchSell.tsx
+++ src/components/SearchSell/SearchSell.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useSearchSell from '../../hooks/useSearchSell';
 import { Input } from '../../theme/styledcomponents';
 import './SearchSell.css';
 import { Card } from '../../types/cardTypes';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../types/stateType';
+import { isCard } from '../../utils/typeGuards';
 
 interface SearchSellProps {
   selectedCard: Card | null;
@@ -15,7 +16,15 @@ const SearchSell: React.FC<SearchSellProps> = ({ selectedCard, setSelectedCard }
   const { query, results, error, setQuery } = useSearchSell();
   const modalDetails = useSelector((state: AppState) => state.modal.modalDetails);
 
-  
+  const [image, setImage] = React.useState<string | null>(null);
+
+  useEffect(() => {
+    if (isCard(modalDetails)) {
+      if (modalDetails.images.small !== image) {
+        setImage(modalDetails.images.small);
+      }
+    }
+  }, [modalDetails, image]);
 
   const handleCardClick = (card: Card) => {
     setSelectedCard(card); 
@@ -23,6 +32,8 @@ const SearchSell: React.FC<SearchSellProps> = ({ selectedCard, setSelectedCard }
 
   return (
     <div className="search-sell-container">
+
+      
 
       <Input
         variant="search"
@@ -36,6 +47,8 @@ const SearchSell: React.FC<SearchSellProps> = ({ selectedCard, setSelectedCard }
       {results.length === 0 && query !== '' && !error && (
         <p className="no-results-message">No cards found</p>
       )}
+
+{ image && <img src={image} alt="Card" className="selected-card-image" /> }
 
       <div className="results">
         {results.map((card) => (
