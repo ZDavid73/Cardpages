e33b6d154a838676f12fffeeaa08d6df3b7999db
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 19:10:46 -0500

    fix(Catalogue): add styled Tittle component for improved title display in Catalogue page

--- src/pages/Catalogue/Catalogue.tsx
+++ src/pages/Catalogue/Catalogue.tsx
@@ -2,6 +2,7 @@ import './Catalogue.css'
 import Header from "../../components/Header/Header"
 import Profile from "../../components/Profile/Profile"
 import AddButton from '../../components/AddButton/AddButton'
+import { Tittle  } from '../../theme/styledcomponents';
 
 const Catalogue = () => {
     return (
@@ -10,6 +11,7 @@ const Catalogue = () => {
            <Profile/>
             <Header/> 
         </div>
+        <Tittle variant="white">Cards On Sale</Tittle>
         <AddButton/>
         </>
     )
