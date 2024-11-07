b5e3ee99f1174648d615b2755764c9b41bc73d3e
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-06 19:14:58 -0500

    feat(SellCardButton): create SellCardButton component for opening card creation modal

--- src/components/AddCard/AddCard.tsx
+++ src/components/AddCard/AddCard.tsx
@@ -1,1 +1,21 @@
+import { Button } from '../../theme/styledcomponents';
+import useModal from '../../hooks/useModal';
+import './SellCardButton.css';
 
+const SellCardButton = () => {
+  const { handleOpen } = useModal();
+
+  const openSellCardModal = () => {
+    handleOpen('createCard'); 
+  };
+
+  return (
+    <div className="sell-card-button">
+      <Button variant="purple" onClick={openSellCardModal}>
+        Sell a Card
+      </Button>
+    </div>
+  );
+};
+
+export default SellCardButton;
