3bed6a3bb7eaacdd41c2397ca568d84bcfbb3668
Dan <daramirezp1112@gmail.com>
2024-11-06 20:39:38 -0500

    feat(tournaments): add logic for array 0

--- src/components/ItemSection/ItemSection.tsx
+++ src/components/ItemSection/ItemSection.tsx
@@ -1,3 +1,5 @@
+import { TournamentState } from '../../features/tournamentSlice';
+import { Text } from '../../theme/styledcomponents';
 import { Tournament } from '../../types/tournamentTypes';
 import { isTournament } from '../../utils/typeGuards';
 import TourThumb from '../TourThumb/TourThumb';
@@ -5,12 +7,20 @@ import './ItemSection.css';
 
 type TourSectionProps = {
     items: Tournament[];
+    state: TournamentState;
 }
 
-const TourSection = ({ items }: TourSectionProps) => {
+const TourSection = ({state, items }: TourSectionProps) => {
     return (
         <section className='item-section'>
-            {items.map((item) => {
+
+            {state.loading && <Text variant='white'>Loading...</Text>}
+
+            {state.error && <Text variant='white'>Oh no! There was an error, try again later</Text>}
+
+            {items.length === 0 && !state.loading && !state.error && <Text variant='white'>Nothing here</Text>}
+
+            {!state.loading && !state.error && items.map((item) => {
                 if (isTournament(item)) {
                     return <TourThumb key={item.id} tournament={item} />;
                 }
