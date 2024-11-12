360c4ed04724b7ce7d6edf02b0dabd6f47bf5eb0
Dan <daramirezp1112@gmail.com>
2024-11-11 19:39:27 -0500

    update(tournaments): update tournament slice with levels

--- src/features/auth/userSlice.ts
+++ src/features/auth/userSlice.ts
@@ -5,12 +5,14 @@ export interface UserState {
     id: string;
     username: string;
     picture: string;
+    level: number;
 }
 
 const initialState: UserState = {
     id: '',
     username: '',
-    picture: ''
+    picture: '',
+    level: 0
 }
 
 export const userSlice = createSlice({
@@ -21,12 +23,14 @@ export const userSlice = createSlice({
       state.id = action.payload.id;
       state.username = action.payload.username;
       state.picture = action.payload.picture;
+      state.level = action.payload.level
     },
     logout: (state) => {
       console.log('logout desde slice');
       state.id = '';
       state.username = '';
       state.picture = '';
+      state.level = 0;
     }
   },
 })
