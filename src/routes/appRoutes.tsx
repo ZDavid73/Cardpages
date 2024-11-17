d4b02845d8e4a037f118c44bd8c038ca92f3d595
Dan <daramirezp1112@gmail.com>
2024-11-17 00:25:57 -0500

    refactor: refactor lazy loading in game page

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -10,7 +10,9 @@ import ProtectedRoutes from './ProtectedRoutes';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import { AppState } from '../types/stateType';
 import Modal from '../components/Modal/Modal';
-import GamePage from '../pages/GamePage/GamePage';
+import React from 'react';
+
+const LazyGamePage = React.lazy(() => import('../pages/GamePage/GamePage'));
 
 const AppRouter = () => {
   const user = useSelector((state: AppState) => state.user);
@@ -35,7 +37,7 @@ const AppRouter = () => {
             <Route path="/tournaments" element={<Tournament />} />
             <Route path="/search" element={<SearchPage/>} />
             <Route path="/about" element={<AboutPage/>} />
-            <Route path="/game" element={<GamePage/>} />
+            <Route path="/game" element={<LazyGamePage/>} />
             
           </Route>
           </Routes>
