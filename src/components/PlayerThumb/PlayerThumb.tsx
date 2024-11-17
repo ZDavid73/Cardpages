4ca697011631da62ad2535765b37da4c52dcf91a
Dan <daramirezp1112@gmail.com>
2024-11-16 23:38:57 -0500

    style(tournament): add animations to player's carousel

--- src/components/PlayerThumb/PlayerThumb.tsx
+++ src/components/PlayerThumb/PlayerThumb.tsx
@@ -1,6 +1,7 @@
 import { UserState } from '../../features/auth/userSlice';
 import { Text } from '../../theme/styledcomponents';
 import './PlayerThumb.css';
+import { TbCards } from 'react-icons/tb';
 
 type PlayerThumbProps = {
     player: UserState;
@@ -10,10 +11,14 @@ type PlayerThumbProps = {
 
 const PlayerThumb = ({ player, deck, isCenter }: PlayerThumbProps) => {
     return (
-        <div className='player-thumb'>
+        <div className={`player-thumb ${isCenter && 'player-center'}`}>
             <img src={player.picture} alt={player.username} />
             <Text variant='white'>{player.username}</Text>
-            <Text variant='purple'>{deck || null}</Text>
+            <Text variant='white' className='player-level'>level {player.level.toString().padStart(2, '0')}</Text>
+            <div className='player-thumb-deck'>
+                <TbCards color='#a71fd0' />
+                <Text variant='purple'>{deck || null}</Text>
+            </div>
         </div>
     );
 }
