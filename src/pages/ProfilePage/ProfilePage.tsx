394a0e7e354a032ffd0dee22979bd8423d88bf08
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2024-11-24 14:23:51 -0500

    feat(profile): integrate header image update functionality in ProfilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
 import { Text, Button } from "../../theme/styledcomponents";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { AppState } from "../../types/stateType";
+import { updateHeaderImage } from "../../features/auth/userSlice"; // Nueva acción importada
 import { FaEdit } from "react-icons/fa";
 import "./ProfilePage.css";
 
-export const headerImageUrl =
-  "https://zyemimihfcilkfzgwsxv.supabase.co/storage/v1/object/public/Header%20Images/pokemon-101-1280x960.webp";
-
 const ProfilePage = () => {
   const user = useSelector((state: AppState) => state.user);
+  const dispatch = useDispatch();
 
   const [username, setUsername] = useState(user.username);
   const [gender, setGender] = useState("");
   const [country, setCountry] = useState("");
 
   const [profileImage, setProfileImage] = useState(user.picture);
-  const [headerImage, setHeaderImage] = useState(headerImageUrl);
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>, type: "profile" | "header") => {
+  const handleImageChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: "profile" | "header"
+  ) => {
     const file = e.target.files?.[0];
     if (file) {
       const reader = new FileReader();
@@ -26,7 +27,8 @@ const ProfilePage = () => {
         if (type === "profile") {
           setProfileImage(reader.result as string);
         } else {
-          setHeaderImage(reader.result as string);
+          // Actualiza la imagen de encabezado en el estado global (Redux)
+          dispatch(updateHeaderImage(reader.result as string));
         }
       };
       reader.readAsDataURL(file);
@@ -39,13 +41,13 @@ const ProfilePage = () => {
       <div
         className="profile-header-image"
         style={{
-          backgroundImage: `url('${headerImageUrl}')`, 
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          width: '60%',
-          height: '50vh',
-          borderRadius: '10px',
+          backgroundImage: `url('${user.headerImageUrl}')`, // Ahora toma la URL del header desde Redux
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "60%",
+          height: "50vh",
+          borderRadius: "10px",
         }}
       >
         <label htmlFor="header-image-upload" className="edit-icon-wrapper">
@@ -60,79 +62,78 @@ const ProfilePage = () => {
         </label>
       </div>
 
-      
-        <div className="profile-image-section">
-          {/* Profile Image */}
-          <div className="profile-image-wrapper">
-            <img src={profileImage} alt="Profile" className="profile-image" />
-            <label htmlFor="profile-image-upload" className="edit-icon-wrapper">
-              <FaEdit className="edit-icon" />
-              <input
-                type="file"
-                id="profile-image-upload"
-                accept="image/*"
-                onChange={(e) => handleImageChange(e, "profile")}
-                className="image-upload-input"
-              />
-            </label>
-          </div>
-          <Text variant="purple">Level: {user.level.toString().padStart(2, "0")}</Text>
-        </div>
-
-        <div className="profile-info-section">
-          <div className="form-group">
-            <label htmlFor="username">
-              <Text variant="white">Username</Text>
-            </label>
+      <div className="profile-image-section">
+        {/* Profile Image */}
+        <div className="profile-image-wrapper">
+          <img src={profileImage} alt="Profile" className="profile-image" />
+          <label htmlFor="profile-image-upload" className="edit-icon-wrapper">
+            <FaEdit className="edit-icon" />
             <input
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="form-input"
+              type="file"
+              id="profile-image-upload"
+              accept="image/*"
+              onChange={(e) => handleImageChange(e, "profile")}
+              className="image-upload-input"
             />
-          </div>
+          </label>
+        </div>
+        <Text variant="purple">Level: {user.level.toString().padStart(2, "0")}</Text>
+      </div>
 
-          <div className="form-group">
-            <label htmlFor="gender">
-              <Text variant="white">Gender</Text>
-            </label>
-            <select
-              id="gender"
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-              className="form-input"
-            >
-              <option value="">Select gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="country">
-              <Text variant="white">Country</Text>
-            </label>
-            <select
-              id="country"
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-              className="form-input"
-            >
-              <option value="">Select country</option>
-              <option value="usa">United States</option>
-              <option value="mexico">Mexico</option>
-              <option value="canada">Canada</option>
-              <option value="uk">United Kingdom</option>
-            </select>
-          </div>
+      <div className="profile-info-section">
+        <div className="form-group">
+          <label htmlFor="username">
+            <Text variant="white">Username</Text>
+          </label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="form-input"
+          />
         </div>
 
-        <div className="profile-actions">
-          <Button variant="purple">Save</Button>
-          <Button variant="gray">Cancel</Button>
+        <div className="form-group">
+          <label htmlFor="gender">
+            <Text variant="white">Gender</Text>
+          </label>
+          <select
+            id="gender"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+            className="form-input"
+          >
+            <option value="">Select gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
         </div>
+
+        <div className="form-group">
+          <label htmlFor="country">
+            <Text variant="white">Country</Text>
+          </label>
+          <select
+            id="country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            className="form-input"
+          >
+            <option value="">Select country</option>
+            <option value="usa">United States</option>
+            <option value="mexico">Mexico</option>
+            <option value="canada">Canada</option>
+            <option value="uk">United Kingdom</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="profile-actions">
+        <Button variant="purple">Save</Button>
+        <Button variant="gray">Cancel</Button>
+      </div>
     </div>
   );
 };
