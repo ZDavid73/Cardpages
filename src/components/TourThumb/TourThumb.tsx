6325f29d64eb4cc5cf0788f1a64ca3b977d84c86
Dan <daramirezp1112@gmail.com>
2024-11-24 20:02:51 -0500

    update(deck): add seller to thumnail

--- src/components/TourThumb/TourThumb.tsx
+++ src/components/TourThumb/TourThumb.tsx
@@ -5,7 +5,6 @@ import './TourThumb.css';
 import { FaLocationPin } from 'react-icons/fa6';
 import { AppState } from '../../types/stateType';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
 
 type TourThumbProps = {
     tournament: Tournament
@@ -14,9 +13,6 @@ type TourThumbProps = {
 const TourThumb =  ({ tournament }: TourThumbProps) => {
     const id = useSelector((state: AppState) => state.user.id);
     const navigate = useNavigate();
-    const { handleGetUserInfo, tempUser } = useAuth();
-    
-    handleGetUserInfo(tournament.host);
 
     return (
         <section 
@@ -40,7 +36,6 @@ const TourThumb =  ({ tournament }: TourThumbProps) => {
             <div className='thumb-tour-info'>
             <Text variant='white'><FaLocationPin/>{tournament.location}</Text>
             <Text variant='white'>{tournament.date}</Text>
-            <Text variant='purple'>by {tempUser?.username}</Text>
             </div>
         </section>
     );
