7adb12204802b2346c6d8fc5cc41d3a86568c803
Dan <daramirezp1112@gmail.com>
2024-11-27 22:41:53 -0500

    feat(purcahses): added page

--- src/pages/Purchases/Purchases.tsx
+++ src/pages/Purchases/Purchases.tsx
@@ -1,11 +1,48 @@
+import { useSelector } from "react-redux";
 import Profile from "../../components/Profile/Profile"
-import { Text } from "../../theme/styledcomponents"
+import { Container, Tittle } from "../../theme/styledcomponents"
+import { AppState } from "../../types/stateType";
+import ItemSection from "../../components/ItemSection/ItemSection";
 
 const Purchases = () => {
+    const user = useSelector((state: AppState) => state.user);
+    const decks = useSelector((state: AppState) => state.decks);
+    const cards = useSelector((state: AppState) => state.cards);
+
     return (
         <>
-        <Profile/>
-        <Text variant='white'>Purchases</Text>
+        <div className="catalogue-sectionheader">
+            <Profile />
+            <div className="calatogues-header">
+            <div
+                className="catalogues-image"
+                style={{
+                backgroundImage: `url('${user.header}')`, 
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                width: '100%',
+                height: '50vh',
+                borderRadius: '10px',
+                }}
+            ></div>
+            <Container variant="small" className="username-calatogues">
+                <Tittle variant="white" className="username-headers">
+                {user.username}'s purchases
+                </Tittle>
+                <Tittle variant="purple" className="number-of-sales">0 purchases</Tittle>
+            </Container>
+            </div>
+        </div> 
+
+        <div className="catalogue-sectioncards">
+            <Tittle variant='white'>Cards Purchased</Tittle>
+            <ItemSection state={cards} items={cards.cards.filter(c => c.buyerId === user.id)}/>
+
+            <Tittle variant='white'>Decks Purchased</Tittle>
+            <ItemSection state={decks} items={decks.decks.filter(d => d.buyerId === user.id)}/>
+        </div>
+       
         </>
     )
 }
