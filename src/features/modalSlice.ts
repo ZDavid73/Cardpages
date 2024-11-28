1ea3b0c6d492e641f8f204a3c37bfe1fbe842b2f
Dan <daramirezp1112@gmail.com>
2024-11-27 22:23:07 -0500

    refactor: modal slice

--- src/features/modalSlice.ts
+++ src/features/modalSlice.ts
@@ -10,8 +10,8 @@ export interface ModalState {
 }
 
 const initialState: ModalState = {
-    isOpen: true,
-    modal: 'createDeck',
+    isOpen: false,
+    modal: null,
     modalDetails: null,
 }
 
